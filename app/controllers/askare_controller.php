fab24c293e520fea02168ac7ef00204f1ad91271
tmkau <tuuli.kauppala@helsinki.fi>
2016-04-12 00:12:50 +0300

    lisäys toimii

--- app/controllers/askare_controller.php
+++ app/controllers/askare_controller.php
@@ -21,13 +21,9 @@ class AskareController extends BaseController {
             ));
         Kint::dump($params);
         $askare->save();
-        Redirect::to('/askare/'.$askare->askare_id, array('message' => 'Askare on nyt muistilistalla!'));
+        Redirect::to('/askare/askarenakyma/'.$askare->askare_id, array('message' => 'Askare on nyt muistilistalla!'));
     }
     
 }
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
